a799c7b68f1a38d63c9729814bbae5b9e09204c2
unknown <jameskagunga2022@gmail.com>
2024-09-04 20:11:20 +0300

    completed homepage

---
+++ Frontend/src/components/Products.jsx
@@ -0,0 +1,18 @@
+import Product from "./Product";
+
+
+const Products = () => {
+  return (
+    <div className="flex flex-wrap mx-[40px]">
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+      
+      
+
+    </div>
+  );
+};
+
+export default Products;
