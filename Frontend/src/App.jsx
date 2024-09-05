274e2195ad9e31f65c7101c7c4467efc4b75db7c
unknown <jameskagunga2022@gmail.com>
2024-09-05 18:16:22 +0300

    worked on product detailed page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from "./components/Navbar";
 import Myaccount from "./pages/Myaccount";
 import { RouterProvider, createBrowserRouter, Outlet } from "react-router-dom";
 import Announcement from "./components/Announcement";
+import Product from "./pages/Product";
 
 function App() {
   const Layout = () => {
@@ -34,6 +35,10 @@ function App() {
         {
           path:"/myaccount",
           element: <Myaccount />
+        },
+        {
+          path:"/product/:productId",
+          element: <Product />
         }
       ]
     },
--- Frontend/src/components/Product.jsx
+++ Frontend/src/components/Product.jsx
@@ -1,8 +1,8 @@
 import StarRatings from 'react-star-ratings';
-const Product = () => {
+const Product = ({img}) => {
   return (
-    <div className="flex flex-col items-center justify-center h-[500px] m-[10px] cursor-pointer">
-        <img src="/serum1.jpg" alt="" className="h-[400px] w-[300px] bg-cover" />
+    <div className="flex flex-col items-center justify-center h-[500px] m-[30px] cursor-pointer">
+        <img src={img} alt="" className="h-[400px] w-[300px] bg-cover" />
         <h2 className="font-semibold text-[18px] w-[300px]">
           Rosehip Seed ,Argan ,Sweet Almond & Vitamin E Oil-Anti-aging
         </h2>
