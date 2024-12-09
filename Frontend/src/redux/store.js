fc6f591b25f097c73b9dcb87f6da41b4bdc04568
James <jameskagunga2022@gmail.com>
2024-12-09 23:39:42 +0300

    some intergrations

--- Frontend/src/redux/store.js
+++ Frontend/src/redux/store.js
@@ -17,10 +17,10 @@ const persistConfig = {
 }
 
 const rootReducer = combineReducers({ user: userReducer, cart: cartReducer });
-const persistReducer = persistReducer(persistConfig, rootReducer);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-    reducer: persistReducer,
+    reducer: persistedReducer,
 
     middleware:(getDefaultMiddleware) => getDefaultMiddleware({
         serializableCheck: {
