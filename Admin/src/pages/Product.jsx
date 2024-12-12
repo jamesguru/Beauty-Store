50af6a01340a14561fc7fe28ea5ebfefca7cb238
James <jameskagunga2022@gmail.com>
2024-12-12 08:05:52 +0300

    Added fetching products from admin and users

--- Admin/src/pages/Product.jsx
+++ Admin/src/pages/Product.jsx
@@ -1,14 +1,55 @@
 import {LineChart} from '@mui/x-charts/LineChart';
+import { useEffect, useState } from 'react';
 import {FaUpload} from 'react-icons/fa';
+import { Link, useLocation } from 'react-router-dom';
+import { userRequest } from '../requestMethods';
 const Product = () => {
+  const location = useLocation();
+  const id = location.pathname.split("/")[2];
+  const [product, setProduct] = useState({});
+  const [inputs, setInputs] = useState({});
+
+  useEffect(() =>{
+
+    const getProduct = async() =>{
+      try {
+
+        const res = await userRequest.get("/products/find/" + id)
+        setProduct(res.data)
+        
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    getProduct()
+
+  },[])
+
+  
+  const handleChange = (e) => {
+    setInputs((prev) => {
+      return { ...prev, [e.target.name]: e.target.value }
+    })
+  }
+
+ const handleUpdate = async() =>{
+  try {
+    await userRequest.put(`/product/${id}`, {...inputs})
+  } catch (error) {
+    console.log(error)
+  }
+ }
   return (
     <div className="p-5 w-[70vw]">
       {/* FIRST PART */}
       <div className="flex items-center justify-between mb-5">
         <h3 className="text-3xl font-semibold">Product</h3>
-        <button className="bg-slate-500 text-white py-2 px-4 rounded">
+        <Link to="/newproduct">
+        <button className="bg-slate-400 p-[10px] font-semibold text-white cursor-pointer">
           Create
         </button>
+        </Link>
       </div>
       {/* SECOND PART */}
       <div className="flex flex-col md:flex-row gap-5">
@@ -36,7 +77,7 @@ const Product = () => {
         <div className="flex-1 bg-white p-5 shadow-lg rounded-lg">
           <div className="flex items-center mb-5">
             <img
-              src="https://images.pexels.com/photos/8101532/pexels-photo-8101532.jpeg?auto=compress&cs=tinysrgb&w=600"
+              src={product.img}
               alt=""
               className="h-20 w-20 rounded-full mr-5"
             />
@@ -49,12 +90,12 @@ const Product = () => {
 
             <div className="flex justify-between">
               <span className="font-semibold">ID:</span>
-              <span>62528291</span>
+              <span>{product._id}</span>
 
             </div>
             <div className="flex justify-between">
               <span className="font-semibold">Sales:</span>
-              <span>625</span>
+              <span>{product._id}</span>
 
             </div>
             <div className="flex justify-between">
@@ -81,7 +122,11 @@ const Product = () => {
               </label>
               <input
                 type="text"
+                name="title"
+                placeholder={product.title}
                 className="w-full p-2 border border-gray-300 rounded"
+
+                onChange={handleChange}
               />
             </div>
             <div>
@@ -90,7 +135,10 @@ const Product = () => {
               </label>
               <input
                 type="text"
+                name="desc"
+                placeholder={product.desc}
                 className="w-full p-2 border border-gray-300 rounded"
+                onChange={handleChange}
               />
             </div>
             <div>
@@ -99,7 +147,10 @@ const Product = () => {
               </label>
               <input
                 type="number"
+                name="originalPrice"
+                placeholder={product.originalPrice}
                 className="w-full p-2 border border-gray-300 rounded"
+                onChange={handleChange}
               />
             </div>
             <div>
@@ -108,7 +159,10 @@ const Product = () => {
               </label>
               <input
                 type="text"
+                name="discountedPrice"
+                placeholder={product.discountedPrice}
                 className="w-full p-2 border border-gray-300 rounded"
+                onChange={handleChange}
               />
             </div>
             <div>
@@ -130,7 +184,7 @@ const Product = () => {
           <div className="flex-1 flex flex-col items-center space-y-5">
             <div className="flex flex-col items-center">
               <img
-                src="https://images.pexels.com/photos/8101532/pexels-photo-8101532.jpeg?auto=compress&cs=tinysrgb&w=600"
+                src={product.img}
                 alt=""
                 className="h-40 w-40 rounded-full mr-5"
               />
@@ -140,7 +194,7 @@ const Product = () => {
                 <FaUpload className="text-2xl text-gray-700" />
               </label>
 
-              <button className='bg-slate-500 text-white py-2 px-4 rounded mt-5'>Update</button>
+              <button className='bg-slate-500 text-white py-2 px-4 rounded mt-5' onClick={handleUpdate}>Update</button>
 
             </div>
           </div>
